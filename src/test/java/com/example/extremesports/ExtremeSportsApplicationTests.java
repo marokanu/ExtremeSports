43e3b6214f97c57267fa283af97d6661824ea4ad
Romanciuc Adrian <romanciucadrian1@gmail.com>
2022-05-18 14:09:24 +0300

    Task 1 : Inceput

---
+++ src/test/java/com/example/extremesports/ExtremeSportsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.extremesports;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExtremeSportsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
